ecffed4d7513b937980d3e220c26ba82f657ece5
Sampson Akwafuo <32974528+Samakwa@users.noreply.github.com>
2018-10-14 22:46:36 -0500

    new

--- New_rss_compa.py
+++ New_rss_compa.py
@@ -2,16 +2,18 @@ import math
 import csv
 import webbrowser
 
-allowedtime= 12 #24  #48
-cluster1 =[]
-cluster2 =[]
-nodes1=[]
-nodes2 =[]
+allowedtime = 12  # 24  #48
+cluster1 = []
+cluster2 = []
+nodes1 = []
+nodes2 = []
 
-#RSS = input ( "Enter Cordinates of RSS; lat, long: ")
+# RSS = input ( "Enter Cordinates of RSS; lat, long: ")
 RSS = (29.779630, -95.436960)
+
+
 def readdata():
-    with open('test1.csv', 'r+') as in_file:
+    with open('Route_Distances.csv', 'r+') as in_file:
         OurPOD = csv.reader(in_file)
         has_header = csv.Sniffer().has_header(in_file.readline())
         in_file.seek(0)  # Rewind.
@@ -29,7 +31,7 @@ def readdata():
 
 
 def distance(RSS, destination):
-    #readdata()
+    # readdata()
     cum_dist = 0
     with open('test1.csv', 'r+') as in_file:
         OurPOD = csv.reader(in_file)
@@ -45,25 +47,24 @@ def distance(RSS, destination):
             # print(x)
             x = float(x)
             y = float(y)
-            destination = [x,y]
+            destination = [x, y]
             lat1, lon1 = RSS
             lat2, lon2 = destination
             RSS = (29.779630, -95.436960)
-            radius = 3959 # Miles
+            radius = 3959  # Miles
 
-            dlat = math.radians(lat2-lat1)
-            dlon = math.radians(lon2-lon1)
-            a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-            * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            dlat = math.radians(lat2 - lat1)
+            dlon = math.radians(lon2 - lon1)
+            a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
+                * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
             d = radius * c
-            print ("Distance from RSS:", d)
-            #return d
+            print("Distance from RSS:", d)
+            # return d
 
+            # Change the starting node after each iteration
 
-            #Change the starting node after each iteration
-
-            RSS = (x,y)
+            RSS = (x, y)
             dlat = math.radians(lat2 - lat1)
             dlon = math.radians(lon2 - lon1)
             a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
@@ -72,11 +73,11 @@ def distance(RSS, destination):
             dist = radius * c
 
             cum_dist = cum_dist + dist
-            #Assume average speed = 55 miles/hr
-            speed =  55
+            # Assume average speed = 55 miles/hr
+            speed = 55
 
             print("Cumulative route distance through nearest neighbor:", cum_dist)
-            time = cum_dist/speed
+            time = cum_dist / speed
             if time < allowedtime:
                 cluster1.append([x, y])
                 nodes1.append(addr)
@@ -87,11 +88,11 @@ def distance(RSS, destination):
 
                 cluster2.append([x, y])
                 nodes2.append(addr)
-    print ("PODs in cluster1:")
+    print("PODs in cluster1:")
     for item in cluster1:
-        print (item)
+        print(item)
     for addr in nodes1:
-        print (addr)
+        print(addr)
     print("PODs in cluster2:")
     for item in cluster2:
         print(item)
@@ -100,9 +101,11 @@ def distance(RSS, destination):
 
 
 def openmap():
-  with open('newroutes.csv', 'w') as out_file:
+    with open('newroutes.csv', 'w') as out_file:
         new_list = csv.writer(out_file)
 
-  webbrowser.open("https://planner.myrouteonline.com/route-planner")
-  webbrowser.open("https://www.google.es/maps/dir/'-95.436960,29.779630'/'-95.063668,29.900089")
-distance(RSS, destination = readdata())
+    webbrowser.open("https://planner.myrouteonline.com/route-planner")
+    webbrowser.open("https://www.google.es/maps/dir/'-95.436960,29.779630'/'-95.063668,29.900089")
+
+
+distance(RSS, destination=readdata())
---
+++ new1014.py
@@ -0,0 +1,110 @@
+import networkx as nx
+import numpy as np
+import pandas as pd
+import json
+import smopy
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = mpl.rcParams['savefig.dpi'] = 300
+
+# load in the Shapefile dataset with NetworkX
+# returns a graph with each node is a geographical location and
+# each edge is info about the road linknig the two nodes
+g = nx.read_shp("data/tl_2013_06_prisecroads.shp")
+
+# This graph is not necessarliy connected, so we take the largest connected subgraph
+# by using the connected_component_subgraphs function.
+# Unfortunately this dataset is not as robust as I had expected.
+# The full graph is about 7200 nodes while the lagest connected subgraph
+# is only about 400 nodes, thus limiting the amount of meaningful routes.
+sg = list(nx.connected_component_subgraphs(g.to_undirected()))[0]
+
+# Starting => Monterey area
+pos0 = (36.6026, -121.9026)
+
+# Taking a break => Los Angeles area
+pos1 = (34.0569, -118.2427)
+
+# End => California/Mexico Boarder
+pos2 = (32.542181, -117.029543)
+
+def get_path(n0, n1):
+    """If n0 and n1 are connected nodes in the graph, this function
+    return an array of point coordinates along the road linking
+    these two nodes."""
+    return np.array(json.loads(sg[n0][n1]['Json'])['coordinates'])
+
+EARTH_R = 6372.8
+def geocalc(lat0, lon0, lat1, lon1):
+    """Return the distance (in km) between two points in
+    geographical coordinates."""
+    lat0 = np.radians(lat0)
+    lon0 = np.radians(lon0)
+    lat1 = np.radians(lat1)
+    lon1 = np.radians(lon1)
+    dlon = lon0 - lon1
+    y = np.sqrt((np.cos(lat1) * np.sin(dlon)) ** 2 + (np.cos(lat0) * np.sin(lat1) - np.sin(lat0) * np.cos(lat1) * np.cos(dlon)) ** 2)
+    x = np.sin(lat0) * np.sin(lat1) + np.cos(lat0) * np.cos(lat1) * np.cos(dlon)
+    c = np.arctan2(y, x)
+    return EARTH_R * c
+
+def get_path_length(path):
+    return np.sum(geocalc(path[1:,0], path[1:,1], path[:-1,0], path[:-1,1]))
+
+# Compute the length of the road segments.
+for n0, n1 in sg.edges_iter():
+    path = get_path(n0, n1)
+    distance = get_path_length(path)
+    sg.edge[n0][n1]['distance'] = distance
+
+nodes = np.array(sg.nodes())
+
+# Get the closest nodes in the graph looking at the Euclidean distance
+pos0_i = np.argmin(np.sum((nodes[:,::-1] - pos0)**2, axis=1))
+pos1_i = np.argmin(np.sum((nodes[:,::-1] - pos1)**2, axis=1))
+pos2_i = np.argmin(np.sum((nodes[:,::-1] - pos2)**2, axis=1))
+
+# Compute the shortest path. Dijkstra's algorithm.
+path1 = nx.shortest_path(sg, source=tuple(nodes[pos0_i]), target=tuple(nodes[pos1_i]), weight='distance')
+path2 = nx.shortest_path(sg, source=tuple(nodes[pos1_i]), target=tuple(nodes[pos2_i]), weight='distance')
+
+# Get a que sheet of the roads
+roads1 = pd.DataFrame([sg.edge[path1[i]][path1[i + 1]] for i in range(len(path1) - 1)], columns=['FULLNAME', 'MTFCC', 'RTTYP', 'distance'])
+roads2 = pd.DataFrame([sg.edge[path2[i]][path2[i + 1]] for i in range(len(path2) - 1)], columns=['FULLNAME', 'MTFCC', 'RTTYP', 'distance'])
+
+roads = roads1.append(roads2)
+
+# Call the map
+map = smopy.Map(pos0, pos2, z=9, margin=.1)
+
+def get_full_path(path):
+    """Return the positions along a path."""
+    p_list = []
+    curp = None
+    for i in range(len(path)-1):
+        p = get_path(path[i], path[i+1])
+        if curp is None:
+            curp = p
+        if np.sum((p[0]-curp)**2) > np.sum((p[-1]-curp)**2):
+            p = p[::-1,:]
+        p_list.append(p)
+        curp = p[-1]
+    return np.vstack(p_list)
+
+# Get the path
+linepath1 = get_full_path(path1)
+linepath2 = get_full_path(path2)
+
+x, y = map.to_pixels(linepath1[:,1], linepath1[:,0])
+h, k = map.to_pixels(linepath2[:,1], linepath2[:,0])
+
+
+plt.figure(figsize=(5,8))
+map.show_mpl()
+# Plot the itinerary.
+plt.plot(x, y, '-k', lw=1.5)
+plt.plot(h, k, '-k', lw=1.5)
+# Mark our two positions.
+plt.plot(x[0], y[0], 'ob', ms=5);
+plt.plot(h[0], k[0], 'og', ms=5);
+plt.plot(h[-1], k[-1], 'or', ms=5);
