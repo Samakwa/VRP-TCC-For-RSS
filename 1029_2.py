5b2cfe48281a1f7c8ef626d769516f03ec003df6
Sampson Akwafuo <32974528+Samakwa@users.noreply.github.com>
2018-11-05 12:10:07 -0600

    new

--- 1029_2.py
+++ 1029_2.py
@@ -3,6 +3,12 @@ from ortools.constraint_solver import pywrapcp
 from ortools.constraint_solver import routing_enums_pb2
 import csv
 import math
+import pandas as pd
+
+
+speed = 50
+max_dist = 3000  #maximum_distance
+time = max_dist/speed
 
 with open('test1_popn.csv', 'r+') as in_file:
     OurPOD = csv.reader(in_file)
@@ -11,64 +17,61 @@ with open('test1_popn.csv', 'r+') as in_file:
     if has_header:
         next(OurPOD)  # Skip header row.
 
+df = pd.read_csv('Route_Distances.csv', delimiter=r',\s+', index_col=0)
+print(df.to_dict())
+
 def create_data():
     # Locations
     num_vehicles = 5
     depot = 0
-    locations = \
-        [(-95.436960, 29.779630),  # RSS, 7777 Washington Ave
-         (-95.52307, 30.020329), (-95.359396, 29.931701),  # row 0
-         (-95.805101, 29.796431), (-95.219793, 29.598091),
-         (-95.533034, 29.932055), (-95.113964, 29.659826),
-         (-95.113964, 29.659826), (-95.245786, 29.595033),
-         (-95.914379, 30.075356), (-95.28119, 30.12724),
-         (-95.264803, 30.1144), (-95.54983, 30.72724),
-         (-95.419336, 30.018464), (-95.172707, 29.981498),
-         (-95.686076, 29.911038), (-95.063668, 29.900089)]
-    num_locations = len(locations)
-    dist_matrix = {}
-
-    for from_node in range(num_locations):
-        dist_matrix[from_node] = {}
-
-        for to_node in range(num_locations):
-            dist_matrix[from_node][to_node] = (
-                route_distance(
-                    locations[from_node],
-                    locations[to_node]))
-    """"    
     with open('test1_popn.csv', 'r+') as in_file:
         OurPOD = csv.reader(in_file)
         has_header = csv.Sniffer().has_header(in_file.readline())
         in_file.seek(0)  # Rewind.
         if has_header:
             next(OurPOD)  # Skip header row.
-    
-    class AutoVivification(dict):
-    #Implementation of perl's autovivification feature.
 
-    def __getitem__(self, item):
-            try:
-                return dict.__getitem__(self, item)
-            except KeyError:
-                value = self[item] = type(self)()
-                return value
+        data = {}
+        for row in OurPOD:
 
-    data = AutoVivification()
-    filename = 'Route_Distances2.csv'
-    with open(filename, 'r+') as f:
-        reader = csv.reader(f, delimiter=',')
-        next(reader)  # skip the header
-        for row in reader:
-            data[row[1]][row[4]] = row[5]
+            locations = [(row[3]), row[4]]
 
-    """
-    
-    return [num_vehicles, depot, locations, dist_matrix]
+            """
+            locations = \
+                [(-95.436960, 29.779630),  # RSS, 7777 Washington Ave
+                (-95.52307, 30.020329), (-95.359396, 29.931701),  # row 0
+                (-95.805101, 29.796431), (-95.219793, 29.598091),
+                (-95.533034, 29.932055), (-95.113964, 29.659826),
+                (-95.113964, 29.659826), (-95.245786, 29.595033),
+                (-95.914379, 30.075356), (-95.28119, 30.12724),
+                (-95.264803, 30.1144), (-95.54983, 30.72724),
+                (-95.419336, 30.018464), (-95.172707, 29.981498),
+                (-95.686076, 29.911038), (-95.063668, 29.900089)]
+                """
+            num_locations = len(locations)
+            dist_matrix = {}
 
 
+            for from_node in range(num_locations):
+                dist_matrix[from_node] = {}
 
+            for to_node in range(num_locations):
+                dist_matrix[from_node][to_node] = (
+                route_distance(
+                    locations[from_node],
+                    locations[to_node]))
+            Popn =[row[5]]
 
+            capacities = [5600]
+
+            data["locations"] = [(l[0] * 1, l[1] * 1) for l in locations]
+            data["num_locations"] = len(data["locations"])
+            data["num_vehicles"] = 10
+            data["depot"] = 0
+            data["demands"] = Popn
+            data["vehicle_capacities"] = capacities
+
+            return data
 
 
 
@@ -109,15 +112,34 @@ def CreateDistanceCallback(dist_matrix):
 
   return dist_callback
 
-"""
+def create_distance_callback(data):
+  #Creates callback to return distance between points
+  _distances = {}
+
+  for from_node in range(data["num_locations"]):
+    _distances[from_node] = {}
+    for to_node in range(data["num_locations"]):
+      if from_node == to_node:
+        _distances[from_node][to_node] = 0
+      else:
+        _distances[from_node][to_node] = (
+            route_distance(data["locations"][from_node],
+                               data["locations"][to_node]))
+
+  def distance_callback(from_node, to_node):
+
+    return _distances[from_node][to_node]
+
+  return distance_callback
+
 def create_demand_callback(data):
-    #Creates callback to get demands at each location.
+    """Creates callback to get demands at each location."""
     def demand_callback(from_node, to_node):
         return data["demands"][from_node]
     return demand_callback
 
 def add_capacity_constraints(routing, data, demand_callback):
-    #Adds capacity constraint"
+    """Adds capacity constraint"""
     capacity = "Capacity"
     routing.AddDimensionWithVehicleCapacity(
         demand_callback,
@@ -125,136 +147,11 @@ def add_capacity_constraints(routing, data, demand_callback):
         data["vehicle_capacities"], # vehicle maximum capacities
         True, # start cumul to zero
         capacity)
-"""
-def add_distance_dimension(routing, distance_callback):
-  """Add Global Span constraint"""
-  distance = 'Distance'
-  maximum_distance = 3000  # Maximum distance per vehicle.
-  routing.AddDimension(
-      distance_callback,
-      0,  # null slack
-      maximum_distance,
-      True,  # start cumul to zero
-      distance)
-  distance_dimension = routing.GetDimensionOrDie(distance)
-  # Try to minimize the max distance among vehicles.
-  distance_dimension.SetGlobalSpanCostCoefficient(100)
-###########
-# Printer #
-###########
+
 def print_solution(data, routing, assignment):
-  """Print routes on console."""
-  total_distance = 0
-  for vehicle_id in range(data["num_vehicles"]):
-    index = routing.Start(vehicle_id)
-    plan_output = 'Route for vehicle {}:\n'.format(vehicle_id)
-    distance = 0
-    while not routing.IsEnd(index):
-      plan_output += ' {} ->'.format(routing.IndexToNode(index))
-      previous_index = index
-      index = assignment.Value(routing.NextVar(index))
-      distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
-    plan_output += ' {}\n'.format(routing.IndexToNode(index))
-    plan_output += 'Distance of route: {}m\n'.format(distance)
-    print(plan_output)
-    total_distance += distance
-  print('Total distance of all routes: {}m'.format(total_distance))
-########
-# Main #
-########
-def main():
-  """Entry point of the program"""
-  # Instantiate the data problem.
-  data = create_data()
-  # Create Routing Model
-  routing = pywrapcp.RoutingModel(
-      data["num_locations"],
-      data["num_vehicles"],
-      data["depot"])
-  # Define weight of each edge
-  distance_callback = create_distance_callback(data)
-  routing.SetArcCostEvaluatorOfAllVehicles(distance_callback)
-  add_distance_dimension(routing, distance_callback)
-  # Setting first solution heuristic (cheapest addition).
-  search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
-  search_parameters.first_solution_strategy = (
-      routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC) # pylint: disable=no-member
-  # Solve the problem.
-  assignment = routing.SolveWithParameters(search_parameters)
-  print_solution(data, routing, assignment)
-if __name__ == '__main__':
-  main()
-"""
-def add_distance_dimension(routing, dist_callback):
-  #Add Global Span constraint
-  distance = "Distance"
-  maximum_distance = 3000
-  routing.AddDimension(
-    dist_callback,
-    0, # null slack
-    maximum_distance, # maximum distance per vehicle
-    True, # start cumul to zero
-    distance)
-  distance_dimension = routing.GetDimensionOrDie(distance)
-  # Try to minimize the max distance among vehicles.
-  distance_dimension.SetGlobalSpanCostCoefficient(100)
-
-
-
-####################
-# Get Routes Array #
-####################
-def get_routes_array(assignment, num_vehicles, routing):
-  # Get the routes for an assignent and return as a list of lists.
-  routes = []
-  for route_nbr in range(num_vehicles):
-    node = routing.Start(route_nbr)
-    route = []
-
-    while not routing.IsEnd(node):
-      index = routing.NodeToIndex(node)
-      route.append(index)
-      node = assignment.Value(routing.NextVar(node))
-    routes.append(route)
-  return routes
-
-########
-# Main #
-########
-
-def main():
-  #Entry point of the program
-  # Instantiate the data problem.
-  [num_vehicles, depot, locations, dist_matrix] = create_data()
-  num_locations = len(locations)
-  # Create Routing Model
-  routing = pywrapcp.RoutingModel(num_locations, num_vehicles, depot)
-  # Define weight of each edge
-  dist_callback = CreateDistanceCallback(dist_matrix)
-  routing.SetArcCostEvaluatorOfAllVehicles(dist_callback)
-  add_distance_dimension(routing, dist_callback)
-  # Setting first solution heuristic (cheapest addition).
-  search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
-  search_parameters.first_solution_strategy = (
-    routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-  # Solve the problem.
-  assignment = routing.SolveWithParameters(search_parameters)
-  routes = get_routes_array(assignment, num_vehicles, routing)
-  print("Routes array:")
-  print(routes)
-
-if __name__ == '__main__':
-  main()
-
-"""
-"""
-###########
-# Printer #
-###########
-def print_solution(data, routing, assignment):
-    #Print routes on console.
+    """Print routes on console."""
     total_dist = 0
-    for vehicle_id in xrange(data["num_vehicles"]):
+    for vehicle_id in range(data["num_vehicles"]):
         index = routing.Start(vehicle_id)
         plan_output = 'Route for vehicle {0}:\n'.format(vehicle_id)
         route_dist = 0
@@ -262,7 +159,7 @@ def print_solution(data, routing, assignment):
         while not routing.IsEnd(index):
             node_index = routing.IndexToNode(index)
             next_node_index = routing.IndexToNode(assignment.Value(routing.NextVar(index)))
-            route_dist += manhattan_distance(
+            route_dist += route_distance(
                 data["locations"][node_index],
                 data["locations"][next_node_index])
             route_load += data["demands"][node_index]
@@ -276,31 +173,30 @@ def print_solution(data, routing, assignment):
         plan_output += 'Load of the route: {0}\n'.format(route_load)
         print(plan_output)
     print('Total Distance of all routes: {0}m'.format(total_dist))
-########
-# Main #
-########
-def main():
-  #Entry point of the program
-  #Instantiate the data problem.
-  data = create_data_model()
-  # Create Routing Model
-  routing = pywrapcp.RoutingModel(
-      data["num_locations"],
-      data["num_vehicles"],
-      data["depot"])
-  # Define weight of each edge
-  distance_callback = create_distance_callback(data)
-  routing.SetArcCostEvaluatorOfAllVehicles(distance_callback)
-# Add Capacity constraint
-  demand_callback = create_demand_callback(data)
-  add_capacity_constraints(routing, data, demand_callback)
-  # Setting first solution heuristic (cheapest addition).
-  search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
-  search_parameters.first_solution_strategy = (
-      routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
-  # Solve the problem.
-  assignment = routing.SolveWithParameters(search_parameters)
-  print_solution(data, routing, assignment)
-if __name__ == '__main__':
- #main()
-"""
+
+    def main():
+        """Entry point of the program"""
+        # Instantiate the data problem.
+        data = create_data_model()
+        # Create Routing Model
+        routing = pywrapcp.RoutingModel(
+            data["num_locations"],
+            data["num_vehicles"],
+            data["depot"])
+        # Define weight of each edge
+        distance_callback = create_distance_callback(data)
+        routing.SetArcCostEvaluatorOfAllVehicles(distance_callback)
+        # Add Capacity constraint
+        demand_callback = create_demand_callback(data)
+        add_capacity_constraints(routing, data, demand_callback)
+        # Setting first solution heuristic (cheapest addition).
+        search_parameters = pywrapcp.RoutingModel.DefaultSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+        # Solve the problem.
+        assignment = routing.SolveWithParameters(search_parameters)
+        if assignment:
+            print_solution(data, routing, assignment)
+
+    if __name__ == '__main__':
+        main()
