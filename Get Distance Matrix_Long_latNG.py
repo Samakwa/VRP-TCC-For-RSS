f4776301a24b072e1710267f66c04a35ac738125
Sampson Akwafuo <32974528+Samakwa@users.noreply.github.com>
2019-09-10 18:08:29 -0500

    Update Get Distance Matrix_Long_latNG.py

--- Get Distance Matrix_Long_latNG.py
+++ Get Distance Matrix_Long_latNG.py
@@ -1,8 +1,5 @@
 
-"""
-https://maps.googleapis.com/maps/api/distancematrix/jso
-https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=6.438722,7.486025&destinations=6.031176%2C7.576712%7C6.0828972C7.475118%7C6.51472%2C7.512057%7C6.388048%2C7.289398%7C6.425608%2C7.
-"""
+
 
 #function builds rows of the distance matrix, using the response returned by the send_request function.
 def build_distance_matrix(response):
